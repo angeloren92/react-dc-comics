aa6199924767a20b84908e2e729877a3b809508c
angeloren92 <angeloren92@gmail.com>
2026-03-06 16:26:12 +0100

    add top footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,9 +1,37 @@
-export default function AppFooter(){
+import digitalComics from '/buy-comics-digital-comics.png'
 
+export default function AppFooter() {
 
   return (
-    <footer> 
-      Footer
+    <footer>
+      <div className="top-footer bg-primary">
+        <div className="container text-white">
+          <figure className="d-flex py-2">
+            <div className="d-flex align-items-center">
+              <img src={digitalComics} alt="" className="img-fluid h-50" />
+              <figcaption className="px-3">DIGITAL COMICS</figcaption>
+            </div>
+            <div className="d-flex align-items-center">
+              <img src={digitalComics} alt="" className="img-fluid h-50" />
+              <figcaption className="px-3">DC MERCHANDISE</figcaption>
+            </div>
+            <div className="d-flex align-items-center">
+              <img src={digitalComics} alt="" className="img-fluid h-50" />
+              <figcaption className="px-3">SUBSCRIPTION</figcaption>
+            </div>
+            <div className="d-flex align-items-center">
+              <img src={digitalComics} alt="" className="img-fluid h-50" />
+              <figcaption className="px-3">COMIC SHOP LOCATOR</figcaption>
+            </div>
+            <div className="d-flex align-items-center">
+              <img src={digitalComics} alt="" className="img-fluid h-50" />
+              <figcaption className="px-3">DC POWER VISA</figcaption>
+            </div>
+          </figure>
+        </div>
+      </div>
+
+      
     </footer>
   )
 }
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,10 +1,9 @@
 export default function AppMain(){
 
-
   return (
     <main className="bg-dark"> 
       <div className="container">
-        <h1 className="text-white py-5 h3">--&gt;Content Goes Here&lt;--</h1>
+        <h1 className="text-white py-5 m-0 h3">--&gt;Content Goes Here&lt;--</h1>
       </div>
     </main>
   )
