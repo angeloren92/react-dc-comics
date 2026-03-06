074e0053ccb4cac16aa285a40a023b2fd466f1ee
angeloren92 <angeloren92@gmail.com>
2026-03-06 15:51:57 +0100

    add main

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,8 +2,10 @@ export default function AppMain(){
 
 
   return (
-    <main> 
-      Main
+    <main className="bg-dark"> 
+      <div className="container">
+        <h1 className="text-white py-5 h3">--&gt;Content Goes Here&lt;--</h1>
+      </div>
     </main>
   )
 }
