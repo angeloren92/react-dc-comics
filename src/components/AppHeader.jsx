1a39fa00dafeff2c1e4993c146866e396c0f6643
angeloren92 <angeloren92@gmail.com>
2026-03-06 15:44:02 +0100

    add header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,9 +1,24 @@
-export default function AppHeader(){
+import dcLogo from '/dc-logo.png';
 
+export default function AppHeader() {
 
-  return (
-    <header> 
-      Header
-    </header>
-  )
+    return (
+        <header>
+            <div className="container d-flex justify-content-between py-3">
+                <img src={dcLogo} alt="Logo DC Comics" />
+                <nav className="navbar nav">
+                    <a className="nav-link" href="#">CHARACTERS</a>
+                    <a className="nav-link" href="#">COMICS</a>
+                    <a className="nav-link" href="#">MOVIES</a>
+                    <a className="nav-link" href="#">TV</a>
+                    <a className="nav-link" href="#">GAMES</a>
+                    <a className="nav-link" href="#">COLLECTIBLES</a>
+                    <a className="nav-link" href="#">VIDEOS</a>
+                    <a className="nav-link" href="#">FANS</a>
+                    <a className="nav-link" href="#">NEWS</a>
+                    <a className="nav-link" href="#">SHOP</a>
+                </nav>
+            </div>
+        </header>
+    )
 }
