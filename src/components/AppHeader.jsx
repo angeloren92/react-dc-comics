edee6b91be9e852bef2a403017003a0c90959a04
angeloren92 <angeloren92@gmail.com>
2026-03-07 13:30:28 +0100

    change navbar with bootstrap

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,21 +4,48 @@ export default function AppHeader() {
 
     return (
         <header>
-            <div className="container d-flex justify-content-between py-3">
-                <img src={dcLogo} alt="Logo DC Comics" />
-                <nav className="navbar nav">
-                    <a className="nav-link" href="#">CHARACTERS</a>
-                    <a className="nav-link" href="#">COMICS</a>
-                    <a className="nav-link" href="#">MOVIES</a>
-                    <a className="nav-link" href="#">TV</a>
-                    <a className="nav-link" href="#">GAMES</a>
-                    <a className="nav-link" href="#">COLLECTIBLES</a>
-                    <a className="nav-link" href="#">VIDEOS</a>
-                    <a className="nav-link" href="#">FANS</a>
-                    <a className="nav-link" href="#">NEWS</a>
-                    <a className="nav-link" href="#">SHOP</a>
-                </nav>
-            </div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container">
+                    <a className="navbar-brand" href="#"><img src={dcLogo} alt="" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse text-end" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">CHRACTERS</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">COMICS</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">MOVIES</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">TV</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">GAMES</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">COLLECTIBLES</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">VIDEOS</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">FANS</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">NEWS</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">SHOP</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
     )
 }
