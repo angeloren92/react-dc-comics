52950c1047d485977066213f4ff6faf3ed382f72
angeloren92 <angeloren92@gmail.com>
2026-03-07 18:24:26 +0100

    add hover on navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,14 +4,16 @@ export default function AppHeader() {
 
     return (
         <header>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container">
-                    <a className="navbar-brand" href="#"><img src={dcLogo} alt="" /></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse text-end" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary p-0">
+                <div className="container align-items-stretch">
+                    <div className="d-flex align-items-center">
+                        <a className="navbar-brand py-4" href="#"><img src={dcLogo} alt="" /></a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center fw-bold h-100">
                             <li className="nav-item">
                                 <a className="nav-link" href="#">CHRACTERS</a>
                             </li>
