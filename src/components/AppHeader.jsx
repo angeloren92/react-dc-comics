2e13beee08ba470c36864bf9ea9a8798d369c9c1
angeloren92 <angeloren92@gmail.com>
2026-03-08 09:50:03 +0100

    adj hover navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -5,9 +5,9 @@ export default function AppHeader() {
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary p-0">
-                <div className="container align-items-stretch">
+                <div className="container align-items-stretch justify-content-between">
+                    <a className="navbar-brand py-4" href="#"><img src={dcLogo} alt="" /></a>
                     <div className="d-flex align-items-center">
-                        <a className="navbar-brand py-4" href="#"><img src={dcLogo} alt="" /></a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -15,7 +15,7 @@ export default function AppHeader() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center fw-bold h-100">
                             <li className="nav-item">
-                                <a className="nav-link" href="#">CHRACTERS</a>
+                                <a className="nav-link" href="#">CHARACTERS</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">COMICS</a>
