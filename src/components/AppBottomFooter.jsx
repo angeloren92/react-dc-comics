e401153e684ba680a6f84b06ff848305fb226b03
angeloren92 <angeloren92@gmail.com>
2026-03-08 12:32:48 +0100

    adj bottom footer

--- src/components/AppBottomFooter.jsx
+++ src/components/AppBottomFooter.jsx
@@ -10,16 +10,18 @@ export default function AppBottomFooter() {
     return (
         <section className="bottom-footer">
             <div className="container d-flex justify-content-between align-items-center flex-wrap">
-                <div className="py-5 flex-shrink-0">
-                    <button className="btn btn-outline-primary text-white rounded-0">SIGN-UPNOW!</button>
+                <div className="my-4 my-md-5 my-lg-6 ">
+                    <button className="btn btn-outline-primary text-white py-2 rounded-0">SIGN-UPNOW!</button>
                 </div>
-                <div className="py-1 py-md-3 py-lg-5">
-                    <a href="#" className="text-decoration-none fw-bold">FOLLOW US</a>
-                    <a href="#"><img src={facebookLogo} alt="facebook logo" /></a>
-                    <a href="#"><img src={twitterLogo} alt="twitter logo" /></a>
-                    <a href="#"><img src={youtubeLogo} alt="youtube logo" /></a>
-                    <a href="#"><img src={pinterestLogo} alt="pinterest logo" /></a>
-                    <a href="#"><img src={periscopeLogo} alt="periscope logo" /></a>
+                <div className="d-flex gap-3 align-items-center my-4 my-md-5 my-lg-6">
+                    <a href="#" className="text-decoration-none fw-bold">FOLLOW&nbsp;US</a>
+                    <div className="d-flex flex-wrap gap-3">
+                        <a href="#"><img src={facebookLogo} alt="facebook logo" /></a>
+                        <a href="#"><img src={twitterLogo} alt="twitter logo" /></a>
+                        <a href="#"><img src={youtubeLogo} alt="youtube logo" /></a>
+                        <a href="#"><img src={pinterestLogo} alt="pinterest logo" /></a>
+                        <a href="#"><img src={periscopeLogo} alt="periscope logo" /></a>
+                    </div>
                 </div>
             </div>
         </section>
