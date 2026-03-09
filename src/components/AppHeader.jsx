f2e1551ae886b2578c2ad4b343e323e6cadcf14b
angeloren92 <angeloren92@gmail.com>
2026-03-09 12:07:42 +0100

    adj header with js

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,11 +2,32 @@ import dcLogo from '/dc-logo.png';
 
 export default function AppHeader() {
 
+    const navLinks = [
+        { id: 1, name: 'characters', url: '#', state: false },
+        { id: 2, name: 'comics', url: '#', state: true },
+        { id: 3, name: 'movies', url: '#', state: false },
+        { id: 4, name: 'tv', url: '#', state: false },
+        { id: 5, name: 'games', url: '#', state: false },
+        { id: 6, name: 'collectribes', url: '#', state: false },
+        { id: 7, name: 'videos', url: '#', state: false },
+        { id: 8, name: 'fans', url: '#', state: false },
+        { id: 9, name: 'news', url: '#', state: false },
+        { id: 10, name: 'shop', url: '#', state: false }
+    ];
+
+    const markup = (arr) => {
+        return arr.map(({ id, name, url, state }) => (
+            <li className="nav-item">
+                <a className={`nav-link ${state === true ? 'isActive' : ''}`} href={url} key={id}>{name.toUpperCase()}</a>
+            </li>
+        ))
+    }
+
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary p-0">
                 <div className="container align-items-stretch justify-content-between">
-                    <a className="navbar-brand py-4" href="#"><img src={dcLogo} alt="" /></a>
+                    <a className="navbar-brand py-4" href="#"><img src={dcLogo} alt="logo dc" /></a>
                     <div className="d-flex align-items-center">
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
@@ -14,36 +35,7 @@ export default function AppHeader() {
                     </div>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center fw-bold h-100">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">CHARACTERS</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">COMICS</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">MOVIES</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">TV</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">GAMES</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">COLLECTIBLES</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">VIDEOS</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">FANS</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">NEWS</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">SHOP</a>
-                            </li>
+                            {markup(navLinks)}
                         </ul>
                     </div>
                 </div>
