c8368f321ec414739a45405e8ce4c55c81f4e0b3
angeloren92 <angeloren92@gmail.com>
2026-03-10 14:37:18 +0100

    add bonus

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -1,11 +1,11 @@
-export default function AppCard(props) {
+export default function AppCard({thumb, title, series}) {
 
   return (
     <div className="col">
       <div className="card rounded-0 border-0">
-        <img className="img-fluid" src={props.thumb} alt={props.title} />
+        <img className="img-fluid" src={thumb} alt={title} />
       </div>
-      <p className="py-2 py-md-3 py-lg-4">{props.series.toUpperCase()}</p>
+      <p className="py-2 py-md-3 py-lg-4">{series.toUpperCase()}</p>
     </div>
   )
 }
